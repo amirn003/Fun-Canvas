b820fc2df8de2f7f1bfe5e7eaedab06ac91b0810
amirn003 <amir.naar@gmail.com>
2023-10-31 15:25:10 +0100

    add draw on the web page

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ ctx.lineCap = 'round';
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+
+function draw(e) {
+  if (!isDrawing) return;
+  console.log(e);
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", () => isDrawing = true);
+canvas.addEventListener("mouseup", () => isDrawing = false);
+canvas.addEventListener("mouseout", () => isDrawing = false);
